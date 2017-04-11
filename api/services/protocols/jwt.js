be3a325f664ad916e88c9794c17fee1191a31d7e
mikaelw <mikael.wang@btcc.com>
2017-04-11 10:52:34 +0800

    Use JWT sub claim for id.

--- api/services/protocols/jwt.js
+++ api/services/protocols/jwt.js
@@ -5,27 +5,17 @@
 
 module.exports = function(req, token, next) {
 
-    sails.models.user.findOne({id: token.id}, function (err, user) {
+    sails.models.user.findOne({id: token.sub}, function (err, user) {
 
-        if (err) {
-            req.flash('error', err.message);
-            return next(err);
-        }
-
-        if (!user) {
-            req.flash('error', "USER_NOT_FOUND");
-            return next("USER_NOT_FOUND");
-        }
+        if (err) { return next(err); }
+        if (!user) { return next(null, false); }
 
         sails.models.passport.findOne({
             protocol : 'local'
             , user     : user.id
         }, function (err, passport) {
 
-            if (err) {
-                req.flash('error', err.message);
-                return next(null, false);
-            }
+            if (err) { return next(err); }
 
             return next(null, user, passport);
         });
